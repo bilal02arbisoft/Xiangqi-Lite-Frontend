3fa398c3868238a7d5df4a9914567648c6938240
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-09-05 14:09:38 +0500

    improved playertimer

--- xiangqi_lite/src/pages/Game/components/PlayerTimer.jsx
+++ xiangqi_lite/src/pages/Game/components/PlayerTimer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PlayerCard from 'pages/Game/components/PlayerCard';
 
 function PlayerTimer({ isFlipped, blackTimeRemaining, redTimeRemaining, redPlayer, blackPlayer }) {
-    // Function to format time
+    
     const formatTime = (time) => {
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
         return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
     };
 
-    // Determine the color based on the flipped state
+    
     const timerColor = isFlipped ? 'red' : 'black';
 
     return (
@@ -30,3 +30,4 @@ function PlayerTimer({ isFlipped, blackTimeRemaining, redTimeRemaining, redPlaye
 }
 
 export default PlayerTimer;
+
